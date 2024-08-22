e27606a9729094bd19b79c715275e95c7694645d
Gleb Avrorin <g.avrorin@gmail.com>
2024-08-22 15:35:53 -0700

    123

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     },
     "baseUrl": "https://www.amazon.com/",
     "viewportWidth": 1920,
-    "viewportHeight": 1080
+    "viewportHeight": 1080,
+    screenshotOnRunFailure: true
   },
 });
